3c92a9e20f19504727ad11b664219433da6ffedc
jay-yoon10 <yeojoon.yoon10@gmail.com>
2022-11-15 10:20:03 +1100

    added testing for basic calculation methods

--- src/test/java/com/company/MenuTest.java
+++ src/test/java/com/company/MenuTest.java
@@ -1,62 +1,69 @@
 package com.company;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MenuTest {
-
 
 
     // Test case for add()
 
     @Test
-    void add() {
-        Menu menu = new Menu();
-        double result = menu.add(2, 3);
-        assertEquals(5, result);
-    }
-    // Test case for sub()
-    @Test
-    void sub() {
-        Menu menu = new Menu();
-        double result = menu.sub(2, 3);
-        assertEquals(-1, result);
-    }
-    // Test case for mul()
-    @Test
-    void mul() {
-        Menu menu = new Menu();
-        double result = menu.mul(2, 3);
-        assertEquals(6, result);
+    void testAdd() {
+        assertEquals(20.0d, (new Menu()).add(10.0d, 10.0d));
+        assertEquals(11.0d, (new Menu()).add(1.0d, 10.0d));
+        assertEquals(10.5d, (new Menu()).add(0.5d, 10.0d));
+        assertEquals(9.5d, (new Menu()).add(-0.5d, 10.0d));
     }
 
-    // Test case for div()
+
+    // Test case for sub() method
+
     @Test
-    void div() {
-        Menu menu = new Menu();
-        double result = menu.div(2, 3);
-        assertEquals(0.6666666666666666, result);
-    }
-    // Test case for exp()
-    @Test
-    void exp() {
-        Menu menu = new Menu();
-        double result = menu.exp(2, 3);
-        assertEquals(8, result);
+    void testSub() {
+        assertEquals(0.0d, (new Menu()).sub(10.0d, 10.0d));
+        assertEquals(-9.0d, (new Menu()).sub(1.0d, 10.0d));
+        assertEquals(-9.5d, (new Menu()).sub(0.5d, 10.0d));
+        assertEquals(-10.5d, (new Menu()).sub(-0.5d, 10.0d));
     }
 
-    // Test case for squareRoot()
+    // Test case for mul() method
+
     @Test
-    void squareRoot() {
-        Menu menu = new Menu();
-        double result = menu.squareRoot(4);
-        assertEquals(2, result);
+    void testMul() {
+        assertEquals(100.0d, (new Menu()).mul(10.0d, 10.0d));
+        assertEquals(10.0d, (new Menu()).mul(1.0d, 10.0d));
+        assertEquals(5.0d, (new Menu()).mul(0.5d, 10.0d));
+        assertEquals(-5.0d, (new Menu()).mul(-0.5d, 10.0d));
+    }
+
+    // Test case for div() method
+
+    @Test
+    void testDiv() {
+        assertEquals(1.0d, (new Menu()).div(10.0d, 10.0d));
+        assertEquals(0.1d, (new Menu()).div(1.0d, 10.0d));
+        assertEquals(0.05d, (new Menu()).div(0.5d, 10.0d));
+        assertEquals(-0.05d, (new Menu()).div(-0.5d, 10.0d));
+    }
+
+    // Test case for exp() method
+
+    @Test
+    void testExp() {
+        assertEquals(1.0E10d, (new Menu()).exp(10.0d, 10.0d));
+        assertEquals(1.0d, (new Menu()).exp(1.0d, 10.0d));
+        assertEquals(9.765625E-4d, (new Menu()).exp(0.5d, 10.0d));
+        assertEquals(9.765625E-4d, (new Menu()).exp(-0.5d, 10.0d));
+    }
+
+    // Test case for squareRoot() methode
+
+    @Test
+    void testSquareRoot() {
+        assertEquals(3.1622776601683795d, (new Menu()).squareRoot(10.0d));
     }
 
 }
